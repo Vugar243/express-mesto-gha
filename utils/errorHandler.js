b758dc3cdf0960eadd6fe438325a023ab3a0e9c1
Vugar Seidov <vugar.sagidov2017@yandex.ru>
2023-12-09 09:12:29 +0500

    Бэкенд версия 9.0

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,18 +1,24 @@
 // utils/errorHandler.js
+
 const handleError = (err, res) => {
   let statusCode;
+  let errorMessage;
 
   if (err.name === 'ValidationError') {
     statusCode = 400;
+    errorMessage = err.message || 'Некорректные данные';
   } else if (err.name === 'CastError') {
     statusCode = 400;
+    errorMessage = 'Некорректный формат данных';
   } else if (err.code === 11000) {
     statusCode = 409;
+    errorMessage = 'Данные уже существуют';
   } else {
     statusCode = 500;
+    errorMessage = 'На сервере произошла ошибка';
   }
 
-  res.status(statusCode).send({ message: `Ошибка: ${err.message || 'Внутренняя ошибка сервера'}` });
+  res.status(statusCode).send({ message: `Ошибка: ${errorMessage}` });
 };
 
 module.exports = handleError;
